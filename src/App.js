1fb785e83bba3ac91f2652d217c96a81feb081ae
Alex Ermolenko <alex.ermolenko1@gmail.com>
2022-06-08 17:34:13 +0300

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+
+import { Routes, Route, NavLink } from "react-router-dom";
+
+import Flights from "./pages/Flights";
+import Packages from "./pages/Packages";
+import Home from "./pages/Home";
+import Hotels from "./pages/Hotels";
+import Hotel from "./pages/Hotel";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav>
+          <NavLink
+            className={`nav-item ${({ isActive }) =>
+              isActive ? "active" : ""}`}
+            to="/"
+          >
+            Home page
+          </NavLink>
+
+          <NavLink
+            className={`nav-item ${({ isActive }) =>
+              isActive ? "active" : ""}`}
+            to="flights"
+          >
+            Flights
+          </NavLink>
+          <NavLink
+            className={`nav-item ${({ isActive }) =>
+              isActive ? "active" : ""}`}
+            to="packages"
+          >
+            Packages
+          </NavLink>
+          <NavLink
+            className={`nav-item ${({ isActive }) =>
+              isActive ? "active" : ""}`}
+            to="hotels"
+          >
+            Hotels
+          </NavLink>
+          <NavLink
+            className={`nav-item ${({ isActive }) =>
+              isActive ? "active" : ""}`}
+            to="ololo"
+          >
+            Fake link
+          </NavLink>
+        </nav>
       </header>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="flights" element={<Flights />} />
+        <Route path="packages" element={<Packages />} />
+        <Route path="hotels" element={<Hotels />}>
+          <Route path=":hotelId" element={<Hotel />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/NotFound.js
@@ -0,0 +1,13 @@
+const NotFound = () => {
+  return (
+    <div>
+      <h1>404</h1>
+      <h2>Page Not Found</h2>
+      <p>
+        The Page you are looking for doesn't exist or an other error occured
+      </p>
+    </div>
+  );
+};
+
+export default NotFound;
